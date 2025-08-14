a0647df4f67f4abed4a45e0f3fb7f689c34ffe13
44544494818 <estagiario28.naj.prfn2@pgfn.gov.br>
2025-08-14 15:07:56 -0300

    Started project

---
+++ src/main/java/br/com/acura/forum_back/ForumBackApplication.java
@@ -0,0 +1,13 @@
+package br.com.acura.forum_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/acura/forum_back/ForumBackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.acura.forum_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
